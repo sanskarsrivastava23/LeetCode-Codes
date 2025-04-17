509ece2ffb2f7fc4c0cdac8fae6c94bbc7164852
Sanskar Srivastava <sanskar23082003@gmail.com>
2025-04-17 23:40:45 +0530

    Candy Code Added

---
+++ Candy.cpp
@@ -0,0 +1,22 @@
+class Solution {
+    public:
+        int candy(vector<int>& ratings) {
+            vector<int> left(ratings.size(), 1), right(ratings.size(), 1);
+            int sum=0;
+            for(int i=1;i<ratings.size();i++)
+            {
+                if(ratings[i]>ratings[i-1])
+                left[i]=left[i-1]+1;
+            }
+            for(int i=ratings.size()-2;i>=0;i--)
+            {
+                if(ratings[i]>ratings[i+1])
+                right[i]=right[i+1]+1;
+            }
+            for(int i=0;i<ratings.size();i++)
+            {
+                sum+=max(left[i], right[i]);
+            }
+            return sum;
+        }
+    };
